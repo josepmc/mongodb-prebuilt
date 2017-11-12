7ce5572fb5b6e37471f5a826a59e959c91c4fcfd
Joshua Wiens <joshuaw@easymetrics.com>
2017-11-11 23:17:28 -0600

    fix: Target temp mongo download dep

--- src/mongodb-prebuilt.ts
+++ src/mongodb-prebuilt.ts
@@ -2,7 +2,7 @@ const Debug: any = require('debug');
 const {Glob} = require("glob");
 import {resolve as resolvePath} from 'path';
 import {homedir as osHomeDir} from 'os';
-import {MongoDBDownload} from 'mongodb-download';
+import {MongoDBDownload} from '@easymetrics/mongodb-download';
 
 
 export class MongoDBPrebuilt {
@@ -74,4 +74,3 @@ export class MongoDBPrebuilt {
   }
   
 }
-
